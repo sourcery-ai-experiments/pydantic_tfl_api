37cd49b07bdb27ee096e67fdcbf9a57716f932b0
Rob Aleck <mnbf9rca@users.noreply.github.com>
2024-07-09 12:29:37 +0000

    Update LineStatus model with optional fields

--- pydantic_tfl_api/models/line_status.py
+++ pydantic_tfl_api/models/line_status.py
@@ -1,20 +1,20 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from .validity_period import ValidityPeriod
 from .disruption import Disruption
 
 class LineStatus(BaseModel):
     id: int = Field(alias='id')
-    line_id: str = Field(alias='lineId')
+    line_id: Optional[str] = Field(None, alias='lineId')
     status_severity: int = Field(alias='statusSeverity')
     status_severity_description: str = Field(alias='statusSeverityDescription')
-    reason: str = Field(alias='reason')
+    reason: Optional[str] = Field(None, alias='reason')
     created: datetime = Field(alias='created')
-    last_update: datetime = Field(alias='lastUpdate')
-    validity_periods: List[ValidityPeriod] = Field(alias='validityPeriods')
-    disruption: List[Disruption] = Field(alias='disruption')
+    last_update: Optional[datetime] = Field(None, alias='lastUpdate')
+    validity_periods: List[ValidityPeriod] = Field(None, alias='validityPeriods')
+    disruptions: Optional[List[Disruption]] = Field([], alias='disruptions')
 
     class Config:
         allow_population_by_field_name = True
