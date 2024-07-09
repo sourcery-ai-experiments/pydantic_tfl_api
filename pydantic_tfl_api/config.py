d3232bce78154db1d8830990b099282f8346606a
Rob Aleck <mnbf9rca@users.noreply.github.com>
2024-07-09 09:34:02 +0000

    add basic model features resued from TfL-python-api package

---
+++ pydantic_tfl_api/config.py
@@ -0,0 +1,43 @@
+# from https://github.com/dhilmathy/TfL-python-api
+# MIT License
+
+# Copyright (c) 2018 Mathivanan Palanisamy
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+base_url = "https://api.tfl.gov.uk/"
+
+endpoints = {
+    'stopPointsByLineId': 'Line/{0}/StopPoints',
+    'lineMetaModes': 'Line/Meta/Modes',
+    'linesByLineId': 'Line/{0}',
+    'linesByMode': 'Line/Mode/{0}',
+    'lineStatus': 'Line/{0}/Status',
+    'lineStatusBySeverity': 'Line/Status/{0}',
+    'routeByLineId': 'Line/{0}/Route',
+    'routeByMode': 'Line/Mode/{0}/Route',
+    'lineDisruptionsByLineId': 'Line/{0}/Disruption',
+    'lineDisruptionsByMode': 'Line/Mode/{0}/Disruption',
+    
+    'stopPointMetaModes': 'StopPoint/Meta/Modes',
+    'stopPointById': 'StopPoint/{0}',
+    'stopPointByMode': 'StopPoint/Mode/{0}',
+
+    'arrivalsByLineId': 'Line/{0}/Arrivals'
+}
---
+++ pydantic_tfl_api/rest_client.py
@@ -0,0 +1,48 @@
+# from https://github.com/dhilmathy/TfL-python-api
+# MIT License
+
+# Copyright (c) 2018 Mathivanan Palanisamy
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+from .config import base_url
+
+class RestClient():
+    """RestClient.
+
+    :param ApiToken api_token: API token to access TfL unified API
+    """
+
+    def __init__(self, api_token):
+        self.api_token = { "app_id": api_token.app_id, "app_key": api_token.app_key }
+
+    def send_request(self, location, params=None):
+        return requests.get(base_url + location + "?" + self._get_query_strings(params))
+
+    def _get_query_strings(self, params):
+        if params is None:
+            params = {}
+        if self.api_token is not None:
+            params.update(self.api_token)
+        return urlencode(params)
